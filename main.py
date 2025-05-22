3d7a45fa15330d46414f9c08f1b111e3851debfd
MichaelPKamm <michael.kamm.5@gmail.com>
2025-05-22 13:33:52 +0200

    ui

--- main.py
+++ main.py
@@ -1,33 +1,25 @@
-# main.py
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QVBoxLayout, QWidget
-from import_gpx import parse_gpx
-from calculate import calculate_lkm
-from result import generate_report
+# import andere Dateien
+# from import_gpx import parse_gpx
+# from calculate import calculate_lkm
+# from result import generate_report
 
-class MainWindow(QMainWindow):
+# import Module
+from PyQt5.QtWidgets import*
+from PyQt5.uic import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtCore import *
+
+class fenster(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Pfadi Butterfly Route Planner")
+        loadUi('UI/possible_UI.ui', self)
 
-        layout = QVBoxLayout()
-        self.import_button = QPushButton("Import GPX File")
-        self.import_button.clicked.connect(self.load_gpx)
+    
 
-        layout.addWidget(self.import_button)
-        container = QWidget()
-        container.setLayout(layout)
-        self.setCentralWidget(container)
 
-    def load_gpx(self):
-        gpx_path, _ = QFileDialog.getOpenFileName(self, "Open GPX File", "", "GPX Files (*.gpx)")
-        if gpx_path:
-            points = parse_gpx(gpx_path)
-            lkm, march_time = calculate_lkm(points)
-            generate_report(points, lkm, march_time)
+app = QApplication ([])
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_())
+window = fenster()
+window.show()
+
+app.exec()
